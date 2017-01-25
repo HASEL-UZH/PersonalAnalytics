7b60ee2e4b085e226df443195ee63ddacb89293c
s-mueller <smueller@ifi.uzh.ch>
2017-01-25 23:55:41 +0100

    Fixed bug in the refresh token process

--- src/client/AM.PA.MonitoringTool/FitbitTracker/Data/FitbitConnector.cs
+++ src/client/AM.PA.MonitoringTool/FitbitTracker/Data/FitbitConnector.cs
@@ -16,6 +16,8 @@ using Shared;
 using FitbitTracker.Data.FitbitModel;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
+using System.Net.Http;
 
 namespace FitbitTracker.Data
 {
@@ -30,7 +32,55 @@ namespace FitbitTracker.Data
         private const string SLEEP_URL = "https://api.fitbit.com/1/user/-/sleep/date/{0}.json";
         private const string REFRESH_URL = "https://api.fitbit.com/oauth2/token";
         private const string DEVICE_URL = "https://api.fitbit.com/1/user/-/devices.json";
-        
+        private const string HEARTRATE_URL = "https://api.fitbit.com/1/user/-/activities/heart/date/{0}/1d.json";
+
+        internal static List<HeartRateDayData> GetHeartrateForDay(DateTimeOffset day)
+        {
+            Tuple<HeartData, bool> result = GetDataFromFitbit<HeartData>(String.Format(HEARTRATE_URL, day.ToString(Settings.FITBIT_FORMAT_DAY)));
+            HeartData heartrateData = result.Item1;
+            bool retry = result.Item2;
+
+            if (heartrateData == null && retry)
+            {
+                heartrateData = GetDataFromFitbit<HeartData>(String.Format(HEARTRATE_URL, day.Year + "-" + day.Month + "-" + day.Day)).Item1;
+            }
+            
+            List<HeartRateDayData> data = new List<HeartRateDayData>();
+
+            if (heartrateData.Activities.Count > 0)
+            {
+
+                foreach (HeartRateZone zone in heartrateData.Activities[0].Value.CustomHeartrateZones)
+                {
+                    data.Add(new HeartRateDayData {
+                        Date = DateTime.ParseExact(heartrateData.Activities[0].DateTime, Settings.FITBIT_FORMAT_DAY, CultureInfo.InvariantCulture),
+                        RestingHeartrate = heartrateData.Activities[0].Value.RestingHeartrate,
+                        CaloriesOut = zone.CaloriesOut,
+                        Max = zone.Max,
+                        Min = zone.Min,
+                        MinutesSpent = zone.Minutes,
+                        Name = zone.Name
+                    });
+                }
+
+                foreach (HeartRateZone zone in heartrateData.Activities[0].Value.HeartRateZones)
+                {
+                    data.Add(new HeartRateDayData
+                    {
+                        Date = DateTime.ParseExact(heartrateData.Activities[0].DateTime, Settings.FITBIT_FORMAT_DAY, CultureInfo.InvariantCulture),
+                        RestingHeartrate = heartrateData.Activities[0].Value.RestingHeartrate,
+                        CaloriesOut = zone.CaloriesOut,
+                        Max = zone.Max,
+                        Min = zone.Min,
+                        MinutesSpent = zone.Minutes,
+                        Name = zone.Name
+                    });
+                }
+            }
+
+            return data;
+        }
+
         internal static SleepData GetSleepDataForDay(DateTimeOffset day)
         {
             Tuple<SleepData, bool> result = GetDataFromFitbit<SleepData>(String.Format(SLEEP_URL, day.Year + "-" + day.Month + "-" + day.Day));
@@ -74,7 +124,7 @@ namespace FitbitTracker.Data
 
                 reader = new StreamReader(data);
                 string response = reader.ReadToEnd();
-
+             
                 T dataObject = JsonConvert.DeserializeObject<T>(response);
                 return Tuple.Create<T, bool>(dataObject, false);
             }
@@ -84,6 +134,11 @@ namespace FitbitTracker.Data
                 {
                     RefreshAccessToken();
                     return Tuple.Create<T, bool>(default(T), true);
+                }
+                else if ((e.Response is HttpWebResponse) && (e.Response as HttpWebResponse).StatusCode.ToString().Equals("429"))
+                {
+                    Console.WriteLine("Too many requests");
+                    return Tuple.Create<T, bool>(default(T), false);
                 }
                 else
                 {
@@ -116,6 +171,10 @@ namespace FitbitTracker.Data
         internal static DateTimeOffset GetLatestSyncDate()
         {
             List<Device> devices = GetDeviceData();
+            if (devices == null)
+            {
+                return DateTimeOffset.MinValue;
+            }
             List<DateTimeOffset> syncTimes = new List<DateTimeOffset>();
             foreach (Device device in devices)
             {
@@ -132,7 +191,8 @@ namespace FitbitTracker.Data
             Console.WriteLine("Access token not valid anymore. Try to refresh access token.");
 
             WebClient client = new WebClient();
-            string accessToken = Database.GetInstance().GetSettingsString(Settings.ACCESS_TOKEN, null);
+            string accessToken = Database.GetInstance().GetSettingsString(Settings.CLIENT_ID, null) + ":" + Database.GetInstance().GetSettingsString(Settings.CLIENT_SECRET, null);
+            accessToken = Base64Encode(accessToken);
             client.Headers.Add("Authorization", "Basic " + accessToken);
             Console.WriteLine(accessToken);
 
@@ -152,6 +212,12 @@ namespace FitbitTracker.Data
             Database.GetInstance().SetSettings(Settings.ACCESS_TOKEN, accessResponse.access_token);
             Database.GetInstance().SetSettings(Settings.REFRESH_TOKEN, accessResponse.refresh_token);
             client.Dispose();
+        }
+
+        internal static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
         }
 
     }
