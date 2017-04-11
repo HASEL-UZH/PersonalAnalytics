64bd3f05df3ccad07e251a11c9af0388f6a20760
Sebastian Müller <smueller@ifi.uzh.ch>
2017-04-11 19:04:34 +0200

    Added test case to replace windows web credentials with dummy data

---
+++ src/client/AM.PA.MonitoringTool/FitbitTrackerTests/SecretStorageTests.cs
@@ -0,0 +1,21 @@
+﻿// Created by Sebastian Mueller (smueller@ifi.uzh.ch) from the University of Zurich
+// Created: 2017-04-11
+// 
+// Licensed under the MIT License.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FitbitTracker.Tests
+{
+    [TestClass()]
+    public class SecretStorageTests
+    {
+        [TestMethod()]
+        public void ReplaceDummyFitbitCredentialsTest()
+        {
+            SecretStorage.SaveFitbitClientID("a");
+            SecretStorage.SaveFitbitClientSecret("b");
+            SecretStorage.SaveFitbitFirstAuthorizationCode("c");
+        }
+    }
+}
