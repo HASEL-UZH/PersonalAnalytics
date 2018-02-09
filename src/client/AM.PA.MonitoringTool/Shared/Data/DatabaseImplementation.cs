0858e27ff62bd9692c372f0a557df7dc85dbb10f
casaout <info@andre-meyer.ch>
2018-02-09 14:54:02 -0800

    + bugfix for ExecuteScalar_Double

--- src/client/AM.PA.MonitoringTool/Shared/Data/DatabaseImplementation.cs
+++ src/client/AM.PA.MonitoringTool/Shared/Data/DatabaseImplementation.cs
@@ -83,9 +83,8 @@ namespace Shared.Data
         }
 
         /// <summary>
-        /// Executes a query scalar. Return 1 in case of an entry, or 0
-        /// in case of no entry or an error.
-        /// Returns an INT.
+        /// Executes a query scalar. Returns the INT in case of an entry, 
+        /// or 0 in case of no entry or an error.
         /// </summary>
         /// <param name="query"></param>
         /// <returns></returns>
@@ -97,8 +96,8 @@ namespace Shared.Data
             var cmd = new SQLiteCommand(query, _connection);
             try
             {
-                var ans = Convert.ToInt32(cmd.ExecuteScalar());
-                return ans;
+                var res = cmd.ExecuteScalar();
+                return (DBNull.Value != res) ? Convert.ToInt32(res) : 0;
             }
             catch (Exception e)
             {
@@ -111,10 +110,22 @@ namespace Shared.Data
             }
         }
 
+
         /// <summary>
-        /// Executes a query scalar. Return 1 in case of an entry, or 0
-        /// in case of no entry or an error
-        /// Returns a DOUBLE.
+        /// Executes a query scalar and returns null if there was an error (or no entry)
+        /// used for settings. Somehow strange?
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public object ExecuteScalar2(string query)
+        {
+            try { return new SQLiteCommand(query, _connection).ExecuteScalar(); }
+            catch { return null; }
+        }
+
+        /// <summary>
+        /// Executes a query scalar. Return the DOUBLE in case of an entry, 
+        /// or 0.0 in case of no entry or an error.
         /// </summary>
         /// <param name="query"></param>
         /// <returns></returns>
@@ -126,8 +137,8 @@ namespace Shared.Data
             var cmd = new SQLiteCommand(query, _connection);
             try
             {
-                var ans = Convert.ToDouble(cmd.ExecuteScalar());
-                return ans;
+                var res = cmd.ExecuteScalar();
+                return (DBNull.Value != res) ? Convert.ToDouble(res) : 0.0;
             }
             catch (Exception e)
             {
@@ -165,18 +176,6 @@ namespace Shared.Data
            // {
 
            // }
-        }
-
-        /// <summary>
-        /// Executes a query scalar and returns null if there was an error (or no entry)
-        /// used for settings. Somehow strange?
-        /// </summary>
-        /// <param name="query"></param>
-        /// <returns></returns>
-        public object ExecuteScalar2(string query)
-        {
-            try { return new SQLiteCommand(query, _connection).ExecuteScalar(); }
-            catch { return null; }
         }
 
         /// <summary>
