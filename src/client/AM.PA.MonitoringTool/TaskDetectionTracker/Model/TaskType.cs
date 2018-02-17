be893e380e8cb1392ddfd0ff2dfa4a94ac479648
André Meyer <casaout@users.noreply.github.com>
2018-02-17 12:00:26 -0800

    + minor wording improvements according to feedbacks

--- src/client/AM.PA.MonitoringTool/TaskDetectionTracker/Model/TaskType.cs
+++ src/client/AM.PA.MonitoringTool/TaskDetectionTracker/Model/TaskType.cs
@@ -21,7 +21,7 @@ namespace TaskDetectionTracker.Model
         Awareness,
         [Description("Planning (scheduling meetings, managing tasks)")]
         Planning,
-        [Description("Observation (related to this TaskDetection study)")]
+        [Description("Study (activities related to this study)")]
         Observation,
         [Description("Development (implementing feature, fixing bugs, code reviews, testing)")]
         Development,
