acd116ba52b04bffc0f6988cd935b07c59b1a391
Rohit Kaushik <kaushikrohit325@gmail.com>
2018-06-20 10:31:08 +0530

    + slack-tracker: Add skeleton code

---
+++ src/client/AM.PA.MonitoringTool/SlackTracker/Daemon.cs
@@ -0,0 +1,81 @@
+﻿using Shared;
+using Shared.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackTracker
+{
+    public sealed class Daemon : BaseTracker, ITracker
+    {
+        #region Itracker Stuff
+
+        public Daemon ()
+        {
+            Name = Settings.Name;
+        }
+
+        public override string GetVersion()
+        {
+            var v = new AssemblyName(Assembly.GetExecutingAssembly().FullName).Version;
+            return Shared.Helpers.VersionHelper.GetFormattedVersion(v);
+        }
+
+        public override bool IsEnabled()
+        {
+            return Database.GetInstance().GetSettingsBool(Settings.TRACKER_ENEABLED_SETTING, Settings.IsEnabledByDefault);
+        }
+
+        public override void Start()
+        {
+
+        }
+
+        public override void Stop()
+        {
+
+        }
+
+        public override void CreateDatabaseTablesIfNotExist()
+        {
+
+        }
+
+        public override void UpdateDatabaseTables(int version)
+        {
+
+        }
+
+        void OnTrackerDisabled()
+        {
+            IsRunning = false;
+            Database.GetInstance().SetSettings(Settings.TRACKER_ENEABLED_SETTING, false);
+        }
+
+        public override List<IVisualization> GetVisualizationsDay(DateTimeOffset date)
+        {
+            return null;
+            // Implement
+        }
+
+        public override List<IVisualization> GetVisualizationsWeek(DateTimeOffset date)
+        {
+            return null;
+            // Implemenet
+        }
+
+        public override List<IFirstStartScreen> GetStartScreens()
+        {
+            return null;
+            //return new List<IFirstStartScreen>() { new FirstStartWindow() };
+        }
+        #endregion
+
+        #region Events and Helpers
+
+        #endregion
+    }
+}
---
+++ src/client/AM.PA.MonitoringTool/SlackTracker/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackTracker
+{
+    internal class Settings
+    {
+        internal const string Name = "Slack Tracker";
+        internal const string TRACKER_ENEABLED_SETTING = "SlackTrackerEnabled";
+
+        internal static readonly bool IsEnabledByDefault = true;
+        internal static readonly bool IsDetailedCollectionEnabled = false; // default: disabled
+    }
+}
