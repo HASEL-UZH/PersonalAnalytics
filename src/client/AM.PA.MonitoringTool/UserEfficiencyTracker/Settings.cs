1c9a2221d1bfc77a3b2b5f42b59afb24425b7046
André Meyer <casaout@users.noreply.github.com>
2018-07-23 16:15:38 -0700

    removed UserEfficiencyTracker from study

--- src/client/AM.PA.MonitoringTool/UserEfficiencyTracker/Settings.cs
+++ src/client/AM.PA.MonitoringTool/UserEfficiencyTracker/Settings.cs
@@ -9,7 +9,7 @@ namespace UserEfficiencyTracker
 {
     public static class Settings
     {
-#if Pilot_TaskDetection_March17
+#if Pilot_TaskDetection_March17 || Pilot_Andre_July18
         public static bool DefaultPopUpIsEnabled = false;
 #else // default:
         public static bool DefaultPopUpIsEnabled = true;
