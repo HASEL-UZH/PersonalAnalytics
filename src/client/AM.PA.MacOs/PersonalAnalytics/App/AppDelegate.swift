df63e149bf7239a9ab382bb4a20941c2afef17cc
Chris Satterfield <chris@satterfield.ca>
2019-03-01 11:06:24 -0800

    disable non essential trackers by default

--- src/client/AM.PA.MacOs/PersonalAnalytics/App/AppDelegate.swift
+++ src/client/AM.PA.MacOs/PersonalAnalytics/App/AppDelegate.swift
@@ -313,8 +313,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         
         TrackerManager.shared.register(tracker: UserInputTracker())
         TrackerManager.shared.register(tracker: ActiveApplicationTracker())
-        TrackerManager.shared.register(tracker: TaskProductivityTracker())
-        TrackerManager.shared.register(tracker: EmotionTracker())
+        //TrackerManager.shared.register(tracker: TaskProductivityTracker())
+        //TrackerManager.shared.register(tracker: EmotionTracker())
         
         
         redirectLogToDocuments()
