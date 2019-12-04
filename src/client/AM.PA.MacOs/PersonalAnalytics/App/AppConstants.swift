7a5b0f4710c8fe3777c5273646c873ba844fcfdf
Roy Rutishauser <roy.rutishauser@gmail.com>
2019-12-04 17:57:30 +0100

    user efficiency summary is now handled inside the tracker + TrackerUserNotificationHandling protocol

--- src/client/AM.PA.MacOs/PersonalAnalytics/App/AppConstants.swift
+++ src/client/AM.PA.MacOs/PersonalAnalytics/App/AppConstants.swift
@@ -12,13 +12,9 @@ enum AppConstants {
     
     static let summaryStateKey = "summaryState"
 
-    static let summarySubmittedNotification = "summaryPosted"
     static let signUpClicked = "signUpHasBeenClicked"
     static let notificationsPersistKey = "userNotificationsWillPersist"
     static let projectBundleName = "PersonalAnalytics"
 
     static let retrospectiveColor = "#007acc"
-
-    static let emotionTrackerNotificationID = "EmotionTrackerNotification"
-
 }
