ef299b75e791c78bb27f0f3242726881c776fc4f
Roy Rutishauser <roy.rutishauser@gmail.com>
2020-01-29 15:24:15 +0100

    implemented mouse middle button, XButton1, XButton2

--- src/client/AM.PA.MacOs/PersonalAnalytics/Trackers/UserInputTracker/MouseActionController.swift
+++ src/client/AM.PA.MacOs/PersonalAnalytics/Trackers/UserInputTracker/MouseActionController.swift
@@ -18,8 +18,7 @@ class MouseActionController{
 
     init(){
         self.lastMouseLocation = NSEvent.mouseLocation //add initial mouse location
-        
-        self.globalEventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [NSEvent.EventTypeMask.leftMouseDown, NSEvent.EventTypeMask.rightMouseDown, NSEvent.EventTypeMask.mouseMoved, NSEvent.EventTypeMask.scrollWheel], handler: self.recordActions) as AnyObject?
+        self.globalEventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [NSEvent.EventTypeMask.leftMouseDown, NSEvent.EventTypeMask.rightMouseDown, NSEvent.EventTypeMask.otherMouseDown, NSEvent.EventTypeMask.mouseMoved, NSEvent.EventTypeMask.scrollWheel], handler: self.recordActions) as AnyObject?
     }
     
     func recordActions(mouseEvent:NSEvent){
@@ -30,6 +29,19 @@ class MouseActionController{
             mouseClickList.append(MouseClickEvent(button: .Left, location:  currentLocation))
         case .rightMouseDown:
              mouseClickList.append(MouseClickEvent(button: .Right, location:  currentLocation))
+        case .otherMouseDown:
+            // middle button
+            if (mouseEvent.buttonNumber == 2) {
+                mouseClickList.append(MouseClickEvent(button: .Middle, location:  currentLocation))
+            }
+            // Xbutton1 (thumb button on the side)
+            else if (mouseEvent.buttonNumber == 3) {
+                mouseClickList.append(MouseClickEvent(button: .XButton1, location:  currentLocation))
+            }
+            // Xbutton2 (thumb button on the side)
+            else if (mouseEvent.buttonNumber == 4) {
+                mouseClickList.append(MouseClickEvent(button: .XButton2, location:  currentLocation))
+            }
         case .mouseMoved:
             let distance = calculateDistance(a: currentLocation, b: lastMouseLocation)
             lastMouseLocation = currentLocation
@@ -46,7 +58,7 @@ class MouseActionController{
         return Int(sqrt(pow(b.x-a.x,2) + pow(b.y-a.y,2)))
     }
     
-    func reset(){        
+    func reset(){
         mouseClickList.removeAll()
         mouseScrollList.removeAll()
         mouseMovementList.removeAll()
