8e9986418468b1f08e33c117a81734b93a292fc2
Philip Hofmann <philip.hofmann@uzh.ch>
2020-06-18 14:02:05 +0200

    Add time statistics to retrospection view

--- src/client/AM.PA.MonitoringTool/FocusSession/Visualizations/TimerButton.cs
+++ src/client/AM.PA.MonitoringTool/FocusSession/Visualizations/TimerButton.cs
@@ -11,6 +11,14 @@ namespace FocusSession.Visualizations
     internal class TimerButton : BaseVisualization, IVisualization
     {
         private readonly System.DateTimeOffset _date;
+        // get the amount of time total focused for today
+        private System.TimeSpan totalDay = Data.Queries.GetFocusTimeFromDay(System.DateTime.Now);
+
+        // get the amount of time total focused for this week
+        private System.TimeSpan totalWeek = Data.Queries.GetFocusTimeFromDay(Controls.Timer.StartOfWeek(System.DayOfWeek.Monday));
+
+        // get the amount of time total focused for this month
+        private System.TimeSpan totalMonth = Data.Queries.GetFocusTimeFromDay(new System.DateTime(System.DateTime.Now.Year, System.DateTime.Now.Month, 1));
         public TimerButton(System.DateTimeOffset date)
         {
             this._date = date;
@@ -45,7 +53,10 @@ namespace FocusSession.Visualizations
             ///
             // TODO maybe show interesting stats here like total amount of time focussed with FocusTimer; total amount of emails answered 
             html += "<table>";
-            html += "<p style='text-align: center;'>This view is in development</p>";
+
+            html += "<p style='text-align: center;'>Total time focused this day: " + totalDay.Hours + " hours and " + totalDay.Minutes + " minutes. </p>";
+            html += "<p style='text-align: center;'>Total time focused this week: " + totalWeek.Hours + " hours and " + totalWeek.Minutes + " minutes. </p>";
+            html += "<p style='text-align: center;'>Total time focused this month: " + totalMonth.Hours + " hours and " + totalMonth.Minutes + " minutes. </p>";
             html += "</table>";
 
             return html;
