bb6509f105331d31f4dd456782e61be081d45040
Sebastian Richner <sebastian@richner.ag>
2024-02-05 15:15:35 +0100

    Add tailwindcss, daisyUI, prettier config

--- src/electron/src/components/HelloWorld.vue
+++ src/electron/src/components/HelloWorld.vue
@@ -10,48 +10,16 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" class="btn" @click="count++">count is {{ count }}</button>
+    <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
-    official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped lang="less">
-.read-the-docs {
-  color: #888;
-}
-// Less Example
-// Variables
-@accent-color: #007acc;
-@link-color: @accent-color;
-@link-color-hover: darken(@link-color, 10%);
-
-// Usage
-a,
-.link {
-  color: @link-color;
-}
-a:hover {
-  color: @link-color-hover;
-}
-.widget {
-  color: #fff;
-  background: @link-color;
-}
-</style>
+<style scoped lang="less"></style>
