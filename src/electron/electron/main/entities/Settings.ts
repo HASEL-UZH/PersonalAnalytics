41586b39d64e60f274212edd07e1a928a8063f9a
Sebastian Richner <sebastian@richner.ag>
2024-02-14 13:44:49 +0100

    Update settings entity, add comments

--- src/electron/electron/main/entities/Settings.ts
+++ src/electron/electron/main/entities/Settings.ts
@@ -13,13 +13,19 @@ import {
 export class Settings extends BaseEntity {
   // This is a hack to ensure that only one entity of this type exists
   // Trying to save a new entity will fail with a unique constraint violation
-  @PrimaryGeneratedColumn({ type: 'int', default: () => '1', nullable: false })
-  public onlyOneEntityShouldExist: 1;
+  @PrimaryGeneratedColumn({ update: false, type: 'int', default: () => '1', nullable: false })
+  public onlyOneEntityShouldExist: number = 1;
 
   @Column({ type: 'text', nullable: false })
   studyName: string;
 
-  @Column({ type: 'text', nullable: false })
+  // This should not be updated after the initial creation
+  // The SettingsService creates a trigger to prevent this
+  @Column({
+    type: 'text',
+    nullable: false,
+    update: false
+  })
   userId: string;
 
   @CreateDateColumn({ name: 'created_at' })
