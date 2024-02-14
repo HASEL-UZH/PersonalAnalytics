c5fab1011c4b9b597390d20f4973cd778ad119ea
Sebastian Richner <sebastian@richner.ag>
2024-02-14 13:41:24 +0100

    Add util method to generate random strings

--- src/electron/electron/main/services/utils/helpers.ts
+++ src/electron/electron/main/services/utils/helpers.ts
@@ -7,3 +7,16 @@ export declare interface Is {
 export const is: Is = {
   dev: !electron.app.isPackaged
 };
+
+export function generateAlphaNumericString(length: number = 0): string {
+  if (length <= 0) {
+    throw new Error('Length must be greater than 0');
+  }
+
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
