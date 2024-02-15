547e2d69da260e42a46282fd2dd76f01c705cf05
Sebastian Richner <sebastian@richner.ag>
2024-02-15 15:03:08 +0100

    Add properties to user input

--- src/electron/electron/main/entities/UserInputEntity.ts
+++ src/electron/electron/main/entities/UserInputEntity.ts
@@ -1,5 +1,23 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import BaseTrackedEntity from './BaseTrackedEntity';
 
 @Entity({ name: 'user_input' })
-export class UserInputEntity extends BaseTrackedEntity {}
+export class UserInputEntity extends BaseTrackedEntity {
+  @Column('int', { nullable: false })
+  keysTotal: number;
+
+  @Column('int', { nullable: false })
+  clickTotal: number;
+
+  @Column('float', { nullable: false })
+  movedDistance: number;
+
+  @Column('float', { nullable: false })
+  scrollDelta: number;
+
+  @Column('datetime', { nullable: false })
+  tsStart: Date;
+
+  @Column('datetime', { nullable: false })
+  tsEnd: Date;
+}
