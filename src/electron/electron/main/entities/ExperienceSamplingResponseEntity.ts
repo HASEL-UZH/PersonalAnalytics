9db1be3dd2413d8ae9ef0db0fbfffe4a6e3c7e3e
Sebastian Richner <sebastian@richner.ag>
2024-02-16 23:47:41 +0100

    Update experience sampling response entity with properties

--- src/electron/electron/main/entities/ExperienceSamplingResponseEntity.ts
+++ src/electron/electron/main/entities/ExperienceSamplingResponseEntity.ts
@@ -1,5 +1,14 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import BaseTrackedEntity from './BaseTrackedEntity';
 
 @Entity({ name: 'experience_sampling_responses' })
-export class ExperienceSamplingResponseEntity extends BaseTrackedEntity {}
+export class ExperienceSamplingResponseEntity extends BaseTrackedEntity {
+  @Column('datetime')
+  promptedAt: Date;
+
+  @Column('text')
+  question: string;
+
+  @Column('text')
+  response: string;
+}
