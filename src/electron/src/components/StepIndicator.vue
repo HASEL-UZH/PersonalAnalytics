e63d5d4a0d58d3b9a5e9238169af12f68ab4cf26
Sebastian Richner <sebastian@richner.ag>
2024-02-22 15:37:11 +0100

    Add generic step indicator component from AB

---
+++ src/electron/src/components/StepIndicator.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  totalSteps: {
+    type: Number,
+    required: true
+  },
+  currentStep: {
+    type: Number,
+    required: true
+  }
+});
+</script>
+<template>
+  <div class="flex flex-row justify-between">
+    <template v-for="i in [...Array(totalSteps).keys()]" :key="i">
+      <div
+        class="ml-1 h-3 w-3 rounded-full bg-neutral-700 transition-all duration-300"
+        :class="{
+          'bg-white': i === currentStep
+        }"
+      />
+    </template>
+  </div>
+</template>
