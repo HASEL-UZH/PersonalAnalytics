7265e8ad21424efc7da5cc7bbf721b9d7fa791c2
Sebastian Richner <sebastian@richner.ag>
2024-02-22 09:51:38 +0100

    Add logs for trigger creation

--- src/electron/electron/main/services/SettingsService.ts
+++ src/electron/electron/main/services/SettingsService.ts
@@ -1,14 +1,22 @@
 import { Settings } from '../entities/Settings';
 import { generateAlphaNumericString } from './utils/helpers';
 import studyConfig from '../../../shared/study.config';
+import { getLogger } from '../../shared/Logger';
+
+const LOG = getLogger('SettingsService');
 
 export class SettingsService {
   public async init(): Promise<void> {
     const isSettingsAlreadyCreated: boolean = await this.isSettingsAlreadyCreated();
 
     if (!isSettingsAlreadyCreated) {
-      await this.createProhibitSubjectIdUpdateTrigger();
-      await this.createDefaultSettings();
+      LOG.info(`Creating default settings`);
+      try {
+        await this.createProhibitSubjectIdUpdateTrigger();
+        await this.createDefaultSettings();
+      } catch (error) {
+        LOG.error(`Error while creating default settings: ${error}`);
+      }
     }
   }
 
@@ -25,11 +33,13 @@ export class SettingsService {
   }
 
   private async createProhibitSubjectIdUpdateTrigger(): Promise<void> {
+    LOG.info(`Creating trigger onUpdateSettingsSubjectId`);
     await Settings.query(`
       create trigger onUpdateSettingsSubjectId before update of subjectId on settings
       begin
         select raise(ABORT, 'Updating settings.subjectId is prohibited');
       end;
     `);
+    LOG.info(`Trigger onUpdateSettingsSubjectId created`);
   }
 }
