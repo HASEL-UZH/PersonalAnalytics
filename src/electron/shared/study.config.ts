b8c5cf57e7c1b442e05b45ca4f769661400b8876
Sebastian Richner <sebastian@richner.ag>
2024-03-01 09:10:26 +0100

    Add another question

--- src/electron/shared/study.config.ts
+++ src/electron/shared/study.config.ts
@@ -26,9 +26,13 @@ const studyConfig: StudyConfiguration = {
       enabled: true,
       scale: 7,
       questions: [
-        'Compared to your normal level of productivity, how productive do you consider the previous session?'
+        'Compared to your normal level of productivity, how productive do you consider the previous session?',
+        'How well did you spend your time in the previous session?'
       ],
-      responseOptions: [['not at all productive', 'moderately productive', 'very productive']],
+      responseOptions: [
+        ['not at all productive', 'moderately productive', 'very productive'],
+        ['not well', 'moderately well', 'very well']
+      ],
       // 3 hours
       intervalInMs: 1000 * 60 * 60 * 3,
       // 10% randomization, so the interval will be between 2.7 and 3.3 hours
