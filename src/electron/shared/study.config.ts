7279a957e33abec9e3dd94e6deee24d9dabea87e
Isabelle Cuber <isabelle.cuber@uzh.ch>
2024-06-10 21:01:21 +0200

    Update study.config.ts, adapt to sleep study

--- src/electron/shared/study.config.ts
+++ src/electron/shared/study.config.ts
@@ -1,15 +1,15 @@
 import { StudyConfiguration } from './StudyConfiguration';
 
 const studyConfig: StudyConfiguration = {
-  name: 'PersonalAnalytics Study',
+  name: 'Sandman Sleep Study',
   shortDescription:
     'PersonalAnalytics is a self-monitoring software developed by the Human Aspects of Software Engineering Lab of the University of Zurich to non-intrusively collect computer interaction data and store it locally on your computer. Every now and then, a self-reflection question asks you about time well spent and perceived productivity. In the future, it will add a retrospection that will visualize and correlate the automatically collected and manually reported data to help you learn more about how you spend your time and your productivity. This software is open source, can be adapted and re-used for your own scientific studies.',
   infoUrl: 'https://github.com/HASEL-UZH/PersonalAnalytics',
   privacyPolicyUrl:
     'https://github.com/HASEL-UZH/PersonalAnalytics/blob/dev-am/documentation/PRIVACY.md',
   uploadUrl: 'https://hasel.dev/upload',
-  contactName: 'Dr. André Meyer',
-  contactEmail: 'study@hasel.dev',
+  contactName: 'Isabelle Cuber',
+  contactEmail: 'cuber@ifi.uzh.ch',
   subjectIdLength: 6,
   dataExportEnabled: true,
   trackers: {
@@ -24,7 +24,7 @@ const studyConfig: StudyConfiguration = {
       intervalInMs: 10000
     },
     experienceSamplingTracker: {
-      enabled: true,
+      enabled: false,
       scale: 7,
       questions: [
         'Compared to your normal level of productivity, how productive do you consider the previous session?',
