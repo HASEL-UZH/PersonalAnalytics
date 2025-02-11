d9f431f6d0f6eaed31b44b32528fc9967cbb0506
André Meyer <info@andre-meyer.ch>
2025-02-11 08:47:10 +0100

    [#376] moved is-dev check to other file (suggested by @royru)

--- src/electron/electron/config/Logger.ts
+++ src/electron/electron/config/Logger.ts
@@ -1,12 +1,11 @@
 import logger from 'electron-log/main';
 import { LogFunctions } from 'electron-log';
 import { LOG_FILE_NAME, LOG_LEVEL } from './logger.config';
-import { is } from '../main/services/utils/helpers';
 
 export const getMainLogger = (loggerName: string): LogFunctions => {
   logger.transports.file.fileName = `${LOG_FILE_NAME.BACKGROUND}.log`;
   logger.transports.file.maxSize = 15 * 1024 * 1024;
-  logger.transports.file.level = is.dev ? 'silly' : LOG_LEVEL.FILE;
+  logger.transports.file.level = LOG_LEVEL.FILE;
   logger.transports.console.level = LOG_LEVEL.CONSOLE;
   return logger.scope(loggerName);
 };
