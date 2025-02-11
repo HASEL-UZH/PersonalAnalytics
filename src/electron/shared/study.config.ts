b5b0c44e7b23ff2ae239340327f9a1a82d6d66a6
SWELL-PA-DEV <swell-dev@d2usp.ch>
2025-02-11 15:47:47 +0100

    initial changes to SWELL-deployment

--- src/electron/shared/study.config.ts
+++ src/electron/shared/study.config.ts
@@ -1,15 +1,14 @@
 import { StudyConfiguration } from './StudyConfiguration';
 
 const studyConfig: StudyConfiguration = {
-  name: 'PersonalAnalytics Study',
+  name: 'SWELL - Student Wellbeing and Learning on Laptops',
   shortDescription:
-    'PersonalAnalytics is a self-monitoring software developed by the Human Aspects of Software Engineering Lab of the University of Zurich to non-intrusively collect computer interaction data and store it locally on your computer. Every now and then, a self-reflection question asks you about time well spent and perceived productivity. In the future, it will add a retrospection that will visualize and correlate the automatically collected and manually reported data to help you learn more about how you spend your time and your productivity. This software is open source, can be adapted and re-used for your own scientific studies.',
-  infoUrl: 'https://github.com/HASEL-UZH/PersonalAnalytics',
-  privacyPolicyUrl:
-    'https://github.com/HASEL-UZH/PersonalAnalytics/blob/dev-am/documentation/PRIVACY.md',
-  uploadUrl: 'https://hasel.dev/upload',
-  contactName: 'Dr. André Meyer',
-  contactEmail: 'study@hasel.dev',
+    'tbd',
+  infoUrl: 'https://mydata-lab.uzh.ch/de/studien/SWELL.html',
+  privacyPolicyUrl: 'https://mydata-lab.uzh.ch/de/studien/SWELL.html',
+  uploadUrl: 'https://hasel.dev/swell-upload',
+  contactName: 'Dr. Malte Doehne, Andreas Baumer, Dr. Andre Meyer',
+  contactEmail: 'swell@d2usp.ch',
   subjectIdLength: 6,
   dataExportEnabled: true,
   dataExportEncrypted: false,
@@ -35,8 +34,7 @@ const studyConfig: StudyConfiguration = {
         ['not at all productive', 'moderately productive', 'very productive'],
         ['not well', 'moderately well', 'very well']
       ],
-      // TODO: Change back to 3 hours
-      intervalInMs: 1000 * 60 * 60 * 1,
+      intervalInMs: 1000 * 60 * 60 * 3, // 3 hours
       // 10% randomization, so the interval will be between 2.7 and 3.3 hours
       samplingRandomization: 0.1
     }
