788811af5f8af5ff002ae4f029113b9f606cdd2b
Grigor Dochev <48100639+grigor-dochev@users.noreply.github.com>
2025-11-17 14:16:00 +0100

    add keys for tracking
    e

--- src/electron/electron/main/entities/UserInputEntity.ts
+++ src/electron/electron/main/entities/UserInputEntity.ts
@@ -20,4 +20,31 @@ export class UserInputEntity extends BaseTrackedEntity {
 
   @Column('datetime', { nullable: false })
   tsEnd: Date;
+
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysLetter?: number;
+  
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysNumber?: number;
+
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysNavigate?: number;
+
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysDelete?: number;
+
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysModifier?: number;
+
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysSpace?: number;
+
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysTab?: number;
+  
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keyEnter?: number;
+
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  keysOther?: number;
 }
