f30b2d0c282c7c6c68f4e7824f3bc11f1a5a9ccf
André Meyer <ameyer@ifi.uzh.ch>
2026-07-03 12:01:43 +0200

    refined study config for release (no longer testing Daily Surveys)

--- src/electron/shared/study.config.ts
+++ src/electron/shared/study.config.ts
@@ -71,25 +71,25 @@ const studyConfig: StudyConfiguration = {
       userDefinedInterval_h: [0.5, 1, 2, 3, 4]
     },
     dailySurveyTracker: {
-      enabled: true,
+      enabled: false,
       surveys: [
         {
           samplingType: 'morning',
           delayInMinutes: 5,
           requireAllAnswers: false,
           questions: [
-            {
-              question: 'How motivated are you to start today?',
-              answerType: 'LikertScale',
-              scale: 7,
-              responseOptions: ['not at all motivated', 'moderately motivated', 'very motivated']
-            },
-            {
-              question: 'What is your main goal for today?',
-              answerType: 'TextResponse',
-              responseOptions: 'singleLine',
-              maxLength: 150
-            }
+            // {
+            //   question: 'How motivated are you to start today?',
+            //   answerType: 'LikertScale',
+            //   scale: 7,
+            //   responseOptions: ['not at all motivated', 'moderately motivated', 'very motivated']
+            // },
+            // {
+            //   question: 'What is your main goal for today?',
+            //   answerType: 'TextResponse',
+            //   responseOptions: 'singleLine',
+            //   maxLength: 150
+            // }
           ]
         },
         {
@@ -97,27 +97,27 @@ const studyConfig: StudyConfiguration = {
           delayInMinutes: -30,
           requireAllAnswers: false,
           questions: [
-            {
-              question: 'Overall, how satisfied are you with your workday?',
-              answerType: 'LikertScale',
-              scale: 5,
-              responseOptions: ['very satisfied', 'satisfied', 'neutral', 'dissatisfied', 'very dissatisfied']
-            },
-            {
-              question: 'How much did you interact with your co-workers today?',
-              answerType: 'SingleChoice',
-              responseOptions: ['not at all', 'rarely', 'sometimes', 'often', 'all the time']
-            },
-            {
-              question: 'Where did you mostly work from?',
-              answerType: 'SingleChoice',
-              responseOptions: ['mostly at the office', 'mostly remotely', 'mostly at home']
-            },
-            {
-              question: 'Which distractions did you experience today?',
-              answerType: 'MultiChoice',
-              responseOptions: ['Notifications', 'Unplanned meetings', 'Co-worker interruptions', 'Context switching', 'Noisy environment', 'Personal matters', 'None']
-            }
+            // {
+            //   question: 'Overall, how satisfied are you with your workday?',
+            //   answerType: 'LikertScale',
+            //   scale: 5,
+            //   responseOptions: ['very satisfied', 'satisfied', 'neutral', 'dissatisfied', 'very dissatisfied']
+            // },
+            // {
+            //   question: 'How much did you interact with your co-workers today?',
+            //   answerType: 'SingleChoice',
+            //   responseOptions: ['not at all', 'rarely', 'sometimes', 'often', 'all the time']
+            // },
+            // {
+            //   question: 'Where did you mostly work from?',
+            //   answerType: 'SingleChoice',
+            //   responseOptions: ['mostly at the office', 'mostly remotely', 'mostly at home']
+            // },
+            // {
+            //   question: 'Which distractions did you experience today?',
+            //   answerType: 'MultiChoice',
+            //   responseOptions: ['Notifications', 'Unplanned meetings', 'Co-worker interruptions', 'Context switching', 'Noisy environment', 'Personal matters', 'None']
+            // }
           ]
         }
       ]
