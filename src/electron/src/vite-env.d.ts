bc3107e86ce65fbfd965851a8f859f25a7afa315
Grigor <gdoch10@gmail.com>
2026-07-24 13:55:48 +0200

    [#439] Update packages based on Dependabot suggestions

--- src/electron/src/vite-env.d.ts
+++ src/electron/src/vite-env.d.ts
@@ -2,12 +2,12 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
 
 interface Window {
   // expose in the `electron/preload/index.ts`
-  ipcRenderer: import('electron').IpcRenderer;
+  ipcRenderer: Pick<import('electron').IpcRenderer, 'invoke'>;
 }
