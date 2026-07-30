f1454420196cf70182ef8fc49d77412419b44047
Grigor <gdoch10@gmail.com>
2026-07-30 15:39:06 +0200

    [#520] Update Node runtime to 24

--- src/electron/src/vite-env.d.ts
+++ src/electron/src/vite-env.d.ts
@@ -9,5 +9,5 @@ declare module '*.vue' {
 
 interface Window {
   // expose in the `electron/preload/index.ts`
-  ipcRenderer: import('electron').IpcRenderer;
+  ipcRenderer: Pick<import('electron').IpcRenderer, 'invoke'>;
 }
