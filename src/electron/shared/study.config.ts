476a8b9c74bc5e5bef820c98c2f8caaa7455a88f
André Meyer <ameyer@ifi.uzh.ch>
2026-09-23 09:17:30 +0200

    disabled dailySurveyTracker (but kept as example for others)

--- src/electron/shared/study.config.ts
+++ src/electron/shared/study.config.ts
@@ -74,58 +74,58 @@ const studyConfig: StudyConfiguration = {
       allowUserToChangeInterval: true,
       userDefinedInterval_h: [0.5, 1, 2, 3, 4]
     },
-    dailySurveyTracker: {
-      enabled: false,
-      surveys: [
-        {
-          samplingType: 'morning',
-          delayInMinutes: 5,
-          requireAllAnswers: false,
-          questions: [
-            // {
-            //   question: 'How motivated are you to start today?',
-            //   answerType: 'LikertScale',
-            //   scale: 7,
-            //   responseOptions: ['not at all motivated', 'moderately motivated', 'very motivated']
-            // },
-            // {
-            //   question: 'What is your main goal for today?',
-            //   answerType: 'TextResponse',
-            //   responseOptions: 'singleLine',
-            //   maxLength: 150
-            // }
-          ]
-        },
-        {
-          samplingType: 'evening',
-          delayInMinutes: -30,
-          requireAllAnswers: false,
-          questions: [
-            // {
-            //   question: 'Overall, how satisfied are you with your workday?',
-            //   answerType: 'LikertScale',
-            //   scale: 5,
-            //   responseOptions: ['very satisfied', 'satisfied', 'neutral', 'dissatisfied', 'very dissatisfied']
-            // },
-            // {
-            //   question: 'How much did you interact with your co-workers today?',
-            //   answerType: 'SingleChoice',
-            //   responseOptions: ['not at all', 'rarely', 'sometimes', 'often', 'all the time']
-            // },
-            // {
-            //   question: 'Where did you mostly work from?',
-            //   answerType: 'SingleChoice',
-            //   responseOptions: ['mostly at the office', 'mostly remotely', 'mostly at home']
-            // },
-            // {
-            //   question: 'Which distractions did you experience today?',
-            //   answerType: 'MultiChoice',
-            //   responseOptions: ['Notifications', 'Unplanned meetings', 'Co-worker interruptions', 'Context switching', 'Noisy environment', 'Personal matters', 'None']
-            // }
-          ]
-        }
-      ]
-    }
+    // dailySurveyTracker: {
+    //   enabled: false,
+    //   surveys: [
+    //     {
+    //       samplingType: 'morning',
+    //       delayInMinutes: 5,
+    //       requireAllAnswers: false,
+    //       questions: [
+    //         {
+    //           question: 'How motivated are you to start today?',
+    //           answerType: 'LikertScale',
+    //           scale: 7,
+    //           responseOptions: ['not at all motivated', 'moderately motivated', 'very motivated']
+    //         },
+    //         {
+    //           question: 'What is your main goal for today?',
+    //           answerType: 'TextResponse',
+    //           responseOptions: 'singleLine',
+    //           maxLength: 150
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       samplingType: 'evening',
+    //       delayInMinutes: -30,
+    //       requireAllAnswers: false,
+    //       questions: [
+    //         {
+    //           question: 'Overall, how satisfied are you with your workday?',
+    //           answerType: 'LikertScale',
+    //           scale: 5,
+    //           responseOptions: ['very satisfied', 'satisfied', 'neutral', 'dissatisfied', 'very dissatisfied']
+    //         },
+    //         {
+    //           question: 'How much did you interact with your co-workers today?',
+    //           answerType: 'SingleChoice',
+    //           responseOptions: ['not at all', 'rarely', 'sometimes', 'often', 'all the time']
+    //         },
+    //         {
+    //           question: 'Where did you mostly work from?',
+    //           answerType: 'SingleChoice',
+    //           responseOptions: ['mostly at the office', 'mostly remotely', 'mostly at home']
+    //         },
+    //         {
+    //           question: 'Which distractions did you experience today?',
+    //           answerType: 'MultiChoice',
+    //           responseOptions: ['Notifications', 'Unplanned meetings', 'Co-worker interruptions', 'Context switching', 'Noisy environment', 'Personal matters', 'None']
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // }
   }
 };
 
